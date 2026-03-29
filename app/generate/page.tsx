96a75d1f4e9baf2a04c1c7dfe0fbefe2cf473db0
prasadshegokar <pvsheg@gmail.com>
2026-03-30 01:31:53 +0530

    bugfix

--- app/generate/page.tsx
+++ app/generate/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useState, useEffect, Suspense } from 'react'
+import { useState, useEffect, useRef, Suspense } from 'react'
 import { useSearchParams } from 'next/navigation'
 import { createClient } from '@/lib/supabase'
 import Navbar from '@/components/Navbar'
